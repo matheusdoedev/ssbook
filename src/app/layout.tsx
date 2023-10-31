d94e27e87bcfa6d03e3e1619bc5911a4ccb4d6d8
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-10-31 10:27:34 -0300

    feat: it updates SEO and turns the app in a PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,27 @@ import { Footer } from '@/components'
 import '@/styles/index.scss'
 
 export const metadata: Metadata = {
-  title: 'SSBook'
+  title: 'SSBook',
+  description: 'É um app onde o usuário pode gerenciar os próprios livros.',
+  keywords: 'Livros, ssbook, gerenciar, ler, emprestar, favoritos, autores',
+  robots: 'index, follow',
+  icons: [
+    { type: 'image/svg+x', url: '/assets/favicon.jpg' },
+    { type: 'image/png', sizes: '32x32', url: '/assets/favicon-32x32.png' },
+    { type: 'image/png', sizes: '16x16', url: '/assets/favicon-16x16.png' },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/assets/apple-touch-icon.png'
+    }
+  ],
+  manifest: '/assets/site.webmanifest',
+  openGraph: {
+    type: 'website',
+    description: 'É um app onde o usuário pode gerenciar os próprios livros.',
+    title: 'SSBook',
+    locale: 'pt-br'
+  }
 }
 
 export default function RootLayout({
